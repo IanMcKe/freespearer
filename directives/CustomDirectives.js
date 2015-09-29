48e45a19aefb576bb07f0e9b63b64f140c158bcc
Ian <student@epicodus.com>
2015-09-29 12:12:28 -0700

    custom directive works

--- directives/CustomDirectives.js
+++ directives/CustomDirectives.js
@@ -22,8 +22,8 @@ freespearer.directive('responsiveNav', function() {
         var menuToggle = $('#js-mobile-menu').unbind();
         $('#js-navigation-menu').removeClass("show");
 
-        menuToggle.on('click', function(e) {
-          e.preventDefault();
+        menuToggle.on('click', function(event) {
+          event.preventDefault();
           $('#js-navigation-menu').slideToggle(function(){
             if($('#js-navigation-menu').is(':hidden')) {
               $('#js-navigation-menu').removeAttr('style');
