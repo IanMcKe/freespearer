2dbe11956c9848c0369b38eadab09a04e2ddf8c7
Ian <student@epicodus.com>
2015-09-29 12:09:01 -0700

    not much progress on custom jquery directive

---
+++ directives/CustomDirectives.js
@@ -0,0 +1,49 @@
+freespearer.directive('debug', function($compile) {
+  return {
+    terminal: true,
+    priority: 100000,
+    link: function(scope, element) {
+      var clone = element.clone();
+      element.attr("style", "color:red");
+      clone.removeAttr("debug");
+      var clonedElement = $compile(clone)(scope);
+      element.after(clonedElement);
+    }
+  }
+});
+
+
+freespearer.directive('responsiveNav', function() {
+  return {
+    restrict:"E",
+    scope: true,
+    link: function(scope, element, attrs) {
+      angular.element(document).ready(function() {
+        var menuToggle = $('#js-mobile-menu').unbind();
+        $('#js-navigation-menu').removeClass("show");
+
+        menuToggle.on('click', function(e) {
+          e.preventDefault();
+          $('#js-navigation-menu').slideToggle(function(){
+            if($('#js-navigation-menu').is(':hidden')) {
+              $('#js-navigation-menu').removeAttr('style');
+            }
+          });
+        });
+      });
+    }
+  }
+});
+
+//How does????
+// freespearer.directive('js-navigation-menu', function() {
+//   return function(scope, element) {
+//     element.find("js-navigation-menu").removeClass("show");
+//   }
+// });
+
+
+// console.log('check');
+// console.log(element);
+// console.log(scope);
+// element.removeClass("show");
